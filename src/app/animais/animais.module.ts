6afa15af3931ecb8dbe76a49e785cc0b777ea1f6
WalterLimaViana <walter.lima.viana@gmail.com>
2022-07-19 15:25:26 -0300

    created comentario method

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -8,9 +8,23 @@ import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent, ComentariosComponent],
-  imports: [CommonModule, AnimaisRoutingModule, CartaoModule],
+  declarations: [
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent,
+  ],
+  imports: [
+    CommonModule,
+    AnimaisRoutingModule,
+    CartaoModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AnimaisModule {}
